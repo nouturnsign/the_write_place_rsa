004d72a3c0b6338cd0518e3a26f944b8bbbdbaf4
nouturnsign <ericwangyy@gmail.com>
2022-05-18 14:35:30 -0400

    Sends email when forgot password

--- app/src/main/java/com/example/readysetappv1/ForgotPasswordActivity.java
+++ app/src/main/java/com/example/readysetappv1/ForgotPasswordActivity.java
@@ -1,13 +1,22 @@
 package com.example.readysetappv1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class ForgotPasswordActivity extends AppCompatActivity {
 
+
+    private static final String TAG = "ForgotPassword";
     private EditText emailToSendTo;
     private Button submitButton;
 
@@ -18,7 +27,23 @@ public class ForgotPasswordActivity extends AppCompatActivity {
 
         emailToSendTo = findViewById(R.id.emailToSendTo);
         submitButton = findViewById(R.id.submitEmail);
+        submitButton.setOnClickListener(this::onSubmitEmail);
 
+    }
+
+    private void onSubmitEmail(View v) {
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        String emailAddress = emailToSendTo.getText().toString();
+
+        auth.sendPasswordResetEmail(emailAddress)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "Email sent.");
+                        }
+                    }
+                });
     }
 
 }
