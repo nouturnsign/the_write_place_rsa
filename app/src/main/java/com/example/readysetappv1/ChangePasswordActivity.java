6ce6d500cf8195f341dd088e3216c4bfb1cf12db
nouturnsign <ericwangyy@gmail.com>
2022-05-18 14:50:45 -0400

    Changes password in ChangePasswordActivity.java

--- app/src/main/java/com/example/readysetappv1/ChangePasswordActivity.java
+++ app/src/main/java/com/example/readysetappv1/ChangePasswordActivity.java
@@ -1,13 +1,20 @@
 package com.example.readysetappv1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class ChangePasswordActivity extends AppCompatActivity {
 
@@ -39,6 +46,24 @@ public class ChangePasswordActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    private void changePassword(String newPassword) {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        user.updatePassword(newPassword)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "User password updated.");
+                            Toast.makeText(ChangePasswordActivity.this, "Changed password.", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Log.w(TAG, "Failed to update user password.", task.getException());
+                            Toast.makeText(ChangePasswordActivity.this, "Failed to change password.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
     private void onSubmitPassword(View v) {
 
         String attemptPassword = password1.getText().toString();
@@ -47,7 +72,7 @@ public class ChangePasswordActivity extends AppCompatActivity {
         if (!attemptPassword.equals(confirmPassword)) {
             Toast.makeText(getApplicationContext(), "Failed: " + attemptPassword + " and " + confirmPassword + " do not match.", Toast.LENGTH_LONG).show();
         } else {
-            // TODO: Firebase stuff
+            changePassword(attemptPassword);
             onReturnToProfile(v);
         }
 
