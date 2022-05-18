a1eeca713d65308e573a114dc39f4f118e95d189
nouturnsign <ericwangyy@gmail.com>
2022-05-18 14:45:02 -0400

    Added logs and toasts to ForgotPasswordActivity.java

--- app/src/main/java/com/example/readysetappv1/ForgotPasswordActivity.java
+++ app/src/main/java/com/example/readysetappv1/ForgotPasswordActivity.java
@@ -3,11 +3,13 @@ package com.example.readysetappv1;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -31,19 +33,28 @@ public class ForgotPasswordActivity extends AppCompatActivity {
 
     }
 
-    private void onSubmitEmail(View v) {
+    private void sendForgotPasswordEmail(String emailAddress) {
         FirebaseAuth auth = FirebaseAuth.getInstance();
-        String emailAddress = emailToSendTo.getText().toString();
-
         auth.sendPasswordResetEmail(emailAddress)
                 .addOnCompleteListener(new OnCompleteListener<Void>() {
                     @Override
                     public void onComplete(@NonNull Task<Void> task) {
                         if (task.isSuccessful()) {
                             Log.d(TAG, "Email sent.");
+                            Toast.makeText(ForgotPasswordActivity.this, "Email sent.", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Log.w(TAG, "Email failed to send.", task.getException());
+                            Toast.makeText(ForgotPasswordActivity.this, "Invalid email.", Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
+    }
+
+    private void onSubmitEmail(View v) {
+        String emailAddress = emailToSendTo.getText().toString();
+        sendForgotPasswordEmail(emailAddress);
+        Intent intent = new Intent(ForgotPasswordActivity.this, LoginActivity.class);
+        startActivity(intent);
     }
 
 }
